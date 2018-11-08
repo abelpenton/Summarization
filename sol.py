838556c79241e9e26668010401ec0dabfa28af77
abelpenton <2235penton@gmail.com>
2018-11-08 09:18:28 -0800

    Implement Similarity function for two sentence

--- sol.py
+++ sol.py
@@ -5,6 +5,23 @@ from pagerank_weighted import pagerank_weighted
 import os.path
 from math import log10
 
+def _get_similarity(s1, s2):
+    words_sentence_one = s1.split()
+    words_sentence_two = s2.split()
+
+    common_word_count = _count_common_words(words_sentence_one, words_sentence_two)
+
+    log_s1 = log10(len(words_sentence_one))
+    log_s2 = log10(len(words_sentence_two))
+
+    if log_s1 + log_s2 == 0:
+        return 0
+
+    return common_word_count / (log_s1 + log_s2)
+
+def _count_common_words(words_sentence_one, words_sentence_two):
+    return len(set(words_sentence_one) & set(words_sentence_two))
+
 
 
 def summarize():
